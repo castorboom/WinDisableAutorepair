c10685c634adca16b51a3c9271e3505bcf4c0379
Loris Castorani <castorboom@gmail.com>
2024-07-30 18:28:33 +0200

    First Commit

---
+++ DisableAutoRepair.bat
@@ -0,0 +1,10 @@
+:: ###############################################################
+:: #                                                             #
+:: #  Autore: castorboom                                         #
+:: #  GitHub: https://github.com/castorboom/WinDisableAutorepair #
+:: #  Made with ♥ from Abruzzo (Italy)                           #
+:: #                                                             #
+:: ###############################################################
+
+bcdedit /set {default} bootstatuspolicy ignoreallfailures
+
